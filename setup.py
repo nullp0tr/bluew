8844d5233f1cad5d5bb938ad6487c7a3d4fb6ad3
Ahmed Alsharif (nullp0tr) <ahmeds2000x@gmail.com>
2017-11-02 16:29:03 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup (
+    name='bluew',
+    version='0.0.1',
+    description='Python Wrapper for Bluetoothctl',
+    url='https://github.com/nullp0tr/Bluew',
+    author='Ahmed Alsharif (nullp0tr)',
+    author_email='ahmed@shnaboo.com',
+    license='MIT',
+    packages=['bluew'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    keywords='bluetooth bluez bluetoothctl wrapper',
+    install_requires=[]
+)
