5a9e8f1cd00590ec7af99d54a96efb683b180f18
Ahmed Alsharif <ahmeds2000x@gmail.com>
2018-02-19 00:26:07 +0100

    Add newest functions to __init__

--- bluew/__init__.py
+++ bluew/__init__.py
@@ -15,7 +15,7 @@ import bluew.errors
 from .api import connect, disconnect, remove
 from .api import pair, info, trust, distrust
 from .api import write_attribute, read_attribute
-from .api import Connection, get_devices
+from .api import Connection, devices, controllers
 from .device import Device
 from .controller import Controller
 
@@ -46,7 +46,8 @@ __all__ = ['__version__',
            'write_attribute',
            'read_attribute',
            'Connection',
-           'get_devices',
+           'devices',
+           'controllers',
            'Device',
            'Controller',
            'errors']
