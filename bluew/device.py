ed70f3f6ce4594030b49fb70634b53ef3c53b2af
Ahmed Alsharif <ahmeds2000x@gmail.com>
2018-04-01 14:29:14 +0200

    Update state on attribute access

--- bluew/device.py
+++ bluew/device.py
@@ -11,20 +11,21 @@ by any EngineBluew when queried for devices.
 """
 
 
+import bluew
 from bluew.ppobj import PPObj
 
 
 class Device(PPObj):  # pylint: disable=too-many-instance-attributes
     """Bluetooth device object."""
+    attrs = {'Adapter', 'Address', 'Alias', 'Appearance',
+             'Blocked', 'Connected', 'LegacyPairing',
+             'Name', 'Paired', 'ServicesResolved', 'Trusted',
+             'UUIDs', 'ManufacturerData', 'RSSI', 'Path',
+             'ServiceData', 'AddressType', 'Class', 'Icon',
+             'Modalias'}
 
     def __init__(self, **kwargs):
-        attrs = {'Adapter', 'Address', 'Alias', 'Appearance',
-                 'Blocked', 'Connected', 'LegacyPairing',
-                 'Name', 'Paired', 'ServicesResolved', 'Trusted',
-                 'UUIDs', 'ManufacturerData', 'RSSI', 'Path',
-                 'ServiceData', 'AddressType', 'Class', 'Icon',
-                 'Modalias'}
-        super().__init__(attrs, **kwargs)
+        super().__init__(Device.attrs, **kwargs)
         self.adapter = kwargs.get('Adapter')
         self.address = kwargs.get('Address')
         self.alias = kwargs.get('Alias')
@@ -45,3 +46,14 @@ class Device(PPObj):  # pylint: disable=too-many-instance-attributes
         self.cls = kwargs.get('Class')
         self.icon = kwargs.get('Icon')
         self.modalias = kwargs.get('Modalias')
+
+    def __getattribute__(self, item):
+        # This hack queries the state of the device on attribute access, and
+        # guarantees that all values returned are in fact up to date.
+        # IMPORTANT NOTE: Thou shall not use `self.attr` access in this method,
+        # or thou shall dive in an infinite recursion as deep as thy stack.
+        # SERIOUSLY: `self.attr` is just gonna call this method again.
+        if item not in Device.attrs:
+            mac = super().__getattribute__('address')
+            self = bluew.info(mac)  # noqa: F841
+        return super().__getattribute__(item)
